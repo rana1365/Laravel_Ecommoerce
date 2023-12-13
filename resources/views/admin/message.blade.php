5168267d385da3fd8dc62d57db2ca9b6f7290d47
MD SOHEL RANA <rana135105@gmail.com>
2023-12-13 18:00:28 +0600

    Sub Category insert module done..!

---
+++ resources/views/admin/message.blade.php
@@ -0,0 +1,23 @@
+
+@if(Session::has('error'))
+<div class="alert alert-danger alert-dismissible">
+
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    <h4>
+        <i class="icon fa fa-ban"></i> Error! {{ Session::get('error') }}
+    </h4>
+
+</div>
+
+@endif
+
+@if(Session::has('success'))
+<div class="alert alert-success alert-dismissible">
+
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    <h6>
+        <i class="icon fa fa-check"></i> Success!<br/><br/> {{ Session::get('success') }}
+    </h6>
+
+</div>
+@endif
